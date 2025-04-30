088145c12093a58ec53b5f14cecb8638918e87a1
Amansahu504 <amansahu040805@gmail.com>
2025-04-30 14:03:51 +0530

    commit 2 : backend activated

--- frontend/src/services/helper.js
+++ frontend/src/services/helper.js
@@ -1,3 +1,3 @@
 // export const Backendurl = "http://localhost:5000";
 export const Backendurl = "https://food-delivery-qi5a.onrender.com";
-// export const Backendurl = "https://mom-s-magic-food-delivery-app.onrender.com";
+
